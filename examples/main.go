688eca99b76339110a92cefc5de647fb903340a3
Didip Kerabat <didipk@gmail.com>
2016-05-25 07:23:35 -0700

    jsutil: provide a global way to enhance the VM.

--- examples/main.go
+++ examples/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/didip/jazz"
+	"github.com/didip/jazz/jsutil"
 	"github.com/robertkrimen/otto"
 )
 
@@ -15,7 +16,13 @@ func gohello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	vm := otto.New()
 
+	// Configure all the global settings
+	jsutil.Configure(vm)
+
+	// Expected output: "Hello World, from Go"
 	http.HandleFunc("/", gohello)
+
+	// Expected output: "Hello World, from Javascript"
 	http.HandleFunc("/js", jazz.JSFuncHandler(vm, "jshandlers/index.js"))
 
 	http.ListenAndServe(":8080", nil)
--- jazz.go
+++ jazz.go
@@ -6,18 +6,13 @@ import (
 	"net/http"
 
 	"github.com/robertkrimen/otto"
-
-	"github.com/didip/jazz/httputil"
-	"github.com/didip/jazz/jsutil"
 )
 
 // JSFuncHandler loads and runs a javascript module that can process http request given request handler function.
 func JSFuncHandler(vm *otto.Otto, jsModule string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		jsutil.ConfigureRequire(vm)
 		vm.Set("request", r)
 		vm.Set("response", w)
-		vm.Set("ResponseUtil", httputil.NewResponseUtil)
 
 		_, err := vm.Run(fmt.Sprintf(`require('%v').handle(request, response);`, jsModule))
 		if err != nil {
